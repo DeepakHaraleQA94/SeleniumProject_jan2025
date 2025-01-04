5683569503157075423c3c82bc90084a43bf7606
Your NameDeepakHaraleQA94 <haraledeepak6.com>
2025-01-04 19:34:44 +0530

    i have added new code in the snapshot class

--- src/main/java/SeleniumBasic/snapshot.java
+++ src/main/java/SeleniumBasic/snapshot.java
@@ -3,5 +3,8 @@ package SeleniumBasic;
 public class snapshot {
  public static void main(String[] args) {
 	System.out.println("hello jala");
+	System.out.println("hello dipak");
+	System.out.println("hello sahil");
 }
+ 
 }
