8407a15269a2e7a98cecbd4b7f100c977a5d3d24
DeepakHaraleQA94 <165259830+DeepakHaraleQA94@users.noreply.github.com>
2025-01-08 22:59:39 +0530

    Update login.java

--- src/test/java/Nokri/login.java
+++ src/test/java/Nokri/login.java
@@ -13,7 +13,7 @@ public class login {
 		driver.get("https://www.naukri.com/mnjuser/homepage?utm_source=google&utm_medium=cpc&utm_campaign=Brand_Login_Register");
 	Thread.sleep(3000);
 		driver.findElement(By.id("usernameField")).sendKeys("haraledeepak84@gmail.com");
-		driver.findElement(By.id("passwordField")).sendKeys("Deepak@94");
+		driver.findElement(By.id("passwordField")).sendKeys("Admin@123");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//button[text()='Login']")).click();
 		Thread.sleep(6000);
